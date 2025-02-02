b72109fb941b51b21851952bbccec64752572ea0
jmarc07 <jean-marc.naounou@epitech.eu>
2025-02-02 11:58:38 +0100

    cc

--- src/app/_models/Project.ts
+++ src/app/_models/Project.ts
@@ -1,11 +1,12 @@
 import { Tag } from "./Tag";
 
-export interface Project{
+export interface Project {
     id: number;
     name: string;
     summary: string;
     description: string;
     projectLink: string;
+    githubLink: string;
     pictures: string[];
     tags: Tag[];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,11 +11,25 @@ import { Project } from '../_models/Project';
 export class HomeComponent implements OnInit{
 
   featuredProject = {} as Project;
+  projects: Project[] = [
+    {
+      id: 1,
+      name: 'Project 1',
+      summary: 'Project 1 summary',
+      description: 'Project 1 description',
+      projectLink: 'https://project1.com',
+       githubLink: 'https://github.com/username/project1',
+      pictures: ['assets/images/portfolio-preview.jpg'],
+      tags: []
+    }
+    // Add more projects as needed
+  ];
 
   constructor(private titleService: Title, private projectService: ProjectsService) {
     this.titleService.setTitle('Jean-marc Naounou - Home');
   }
   ngOnInit(): void {
     this.featuredProject = this.projectService.GetProjectById(0);
+    this.projects = this.projectService.GetProjects();
   }
 }
